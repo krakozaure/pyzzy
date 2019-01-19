22eb4c8c6a93f2e6680ba0fd67e593222f9e213a
krakozaure <>
2019-01-19 02:08:07 +0100

    Change classifiers type to list in `setup.py`

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=["colorama", "ruamel.yaml", "toml"],
     tests_require=["pytest", "tox"],
-    classifiers=(
+    classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
@@ -26,5 +26,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",
-    ),
+    ],
 )
