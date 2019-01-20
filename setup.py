bfc834abd85d5b80e4d9fc292e8fcabec3a47d8c
krakozaure <>
2019-01-20 18:53:38 +0100

    Fix bad configuration for packaging
    The `tests` directory was installed as package.
    Some files were also included/excluded due to wrong configuration of `MANIFEST.in`.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/krakozaure/pyzzy",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=["colorama", "ruamel.yaml", "toml"],
     tests_require=["pytest", "tox"],
     classifiers=[
